51a8f73f50f41f7c0a26f61f8bd4a2583f25f8ef
envincebal <vincentyan1@gmail.com>
2019-05-10 20:55:02 -0700

    Added bookshelf container and bookshelf titles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="bookshelf">
+        <div className="currently-reading">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+
+          </div>
+        </div>
+        <div className="want-to-read">
+          <h2 className="bookshelf-title">Want To Read</h2>
+          <div className="bookshelf-books">
+
+          </div>
+        </div>
+        <div className="Read">
+          <h2 className="bookshelf-title">Read</h2>
+          <div className="bookshelf-books">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
