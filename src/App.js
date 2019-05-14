42fa4c946de689637f18d1971319dfd7edd28362
envincebal <vincentyan1@gmail.com>
2019-05-13 20:37:13 -0700

    Added Font Awesome and search page.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="bookshelf">
-        <div className="currently-reading">
-          <h2 className="bookshelf-title">Currently Reading</h2>
-          <div className="bookshelf-books">
+class BooksApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSearchPage: true
+    }
+  }
+  render() {
+    const { showSearchPage } = this.state;
+    return (
+      <div className="App">
 
+        {showSearchPage ? (
+          <div className="search-books">
+            <div className="search-bar">
+              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}><i className="fas fa-arrow-left"></i></button>
+              <input type="text" placeholder="Search by title or author" />
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
           </div>
-        </div>
-        <div className="want-to-read">
-          <h2 className="bookshelf-title">Want To Read</h2>
-          <div className="bookshelf-books">
+        ) : (
+            <div className="list-books">
+              <Header />
+              <div className="bookshelf">
 
-          </div>
-        </div>
-        <div className="Read">
-          <h2 className="bookshelf-title">Read</h2>
-          <div className="bookshelf-books">
+                <div className="currently-reading">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
 
-          </div>
-        </div>
+                  </div>
+                </div>
+                <div className="want-to-read">
+                  <h2 className="bookshelf-title">Want To Read</h2>
+                  <div className="bookshelf-books">
+
+                  </div>
+                </div>
+                <div className="Read">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          )
+        }
+
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
-export default App;
+export default BooksApp;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/index.css';
-import App from './App';
+import BooksApp from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BooksApp />, document.getElementById('root'));
 
