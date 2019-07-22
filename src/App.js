ee9c950523d690218a82b40546400fd48eeded96
envincebal <vincentyan1@gmail.com>
2019-07-21 22:37:03 -0700

    Added book containers in ordered list.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class BooksApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showSearchPage: true
+      showSearchPage: false
     }
   }
   render() {
@@ -31,27 +31,156 @@ class BooksApp extends Component {
                 <div className="currently-reading">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
-
+                    <ol className="books-grid">
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                    </ol>
                   </div>
                 </div>
                 <div className="want-to-read">
                   <h2 className="bookshelf-title">Want To Read</h2>
                   <div className="bookshelf-books">
-
+                    <ol className="books-grid">
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                    </ol>
                   </div>
                 </div>
                 <div className="Read">
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
-
+                    <ol className="books-grid">
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="book">
+                          <div className="book-cover">
+                            <img src="" alt="" />
+                          </div>
+                          <div className="book-menu">
+                            <select>
+                              <option value="move" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                          <p className="book-title"></p>
+                          <p className="book-author"></p>
+                        </div>
+                      </li>
+                    </ol>
                   </div>
                 </div>
               </div>
+              <div className="open-search">
+                <button onClick={() => this.setState({ showSearchPage: true })}>+</button>
+              </div>
             </div>
-
-          )
-        }
-
+          )}
       </div>
     );
   }
